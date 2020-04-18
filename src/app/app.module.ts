92c889297956bcfc003a3ef9d6d8de3707061a06
Marc <seacuke23@gmail.com>
2020-04-18 16:36:51 -0600

    Modified category updating and added transaction ignoring.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,7 +15,8 @@ import {ApiModule} from 'something'
   imports: [
     BrowserModule,
     HttpClientModule,
-    ApiModule
+    ApiModule,
+    FormsModule
 //    ApiModule.forRoot(IdentityService.getApiConfiguration);
   ],
   providers: [],
--- src/app/transaction-table/transaction-table.component.ts
+++ src/app/transaction-table/transaction-table.component.ts
@@ -6,24 +6,49 @@ import { TransactionControllerService, TransactionCategoryControllerService, Tra
   templateUrl: './transaction-table.component.html',
   styleUrls: ['./transaction-table.component.css']
 })
-export class TransactionTableComponent implements OnInit {
-  transactions: Transaction[]=[];
-  categories: TransactionCategory[]=[];
 
-	constructor(private transactionService: TransactionControllerService, private categoryService: TransactionCategoryControllerService) { }
-	ngOnInit() {
-		this.transactionService.getTransactionUsingGET1(true).subscribe((data)=>{  
+
+
+export class TransactionTableComponent implements OnInit {
+  transactions: Transaction[] = [];
+  categories: TransactionCategory[] = [];
+  readonly VERIFIED_STATE: Transaction.StateEnum = Transaction.StateEnum.VERIFIED;
+  readonly IGNORED_STATE: Transaction.StateEnum = Transaction.StateEnum.IGNORE;
+
+  constructor(private transactionService: TransactionControllerService, private categoryService: TransactionCategoryControllerService) { }
+  ngOnInit() {
+    this.transactionService.getTransactions(true).subscribe((data) => {
       console.log(data);
-      this.transactions=data;  
-		})  
-		this.categoryService.getCategoriesUsingGET().subscribe((data)=>{  
+      this.transactions = data;
+    })
+    this.categoryService.getTransactionCategories().subscribe((data) => {
       console.log(data);
-      this.categories=data;  
-		})  
+      this.categories = data;
+      let cat: TransactionCategory = { id: null, name: "select category" };
+      this.categories.unshift(cat);
+    })
   }
 
-  public updateCategory(value: String, transaction: Transaction){
-    console.log("updating category to '" + value + "' for transaction: " + transaction.detail1);
+  public updateTransaction(transaction: Transaction, category: number = null, state: Transaction.StateEnum = null) {
+    let originalTransaction: Transaction = Object.assign({}, transaction);
+    if (category !== null) {
+      transaction.category = category;
+    }
+    if (state !== null) {
+      transaction.state = state;
+    }
+    console.log("Updating from/to");
+    console.log(JSON.stringify(originalTransaction));
+    console.log(JSON.stringify(transaction));
+    this.transactionService.updateTransaction(transaction, transaction.id).subscribe(
+      (data) => {
+      },
+      (err) => {
+        console.log(err);
+        console.log("ya done fukt up a-aron");
+        Object.assign(transaction, originalTransaction);
+      });
+    console.log("Updated");
   }
 
 }
